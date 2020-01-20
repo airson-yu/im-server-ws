6cfdab0fa20fadf61e65c5cd13b80440ddc8fabb
yurh <airson_yu@163.com>
2020-01-20 18:03:19 +0800

    t

--- src/main/java/cc/airson/im/server/ws/ImServerWsApplication.java
+++ src/main/java/cc/airson/im/server/ws/ImServerWsApplication.java
@@ -3,7 +3,9 @@ package cc.airson.im.server.ws;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @MapperScan("cc.airson.im.server.ws.dao.mapper")
 @SpringBootApplication
 public class ImServerWsApplication {
